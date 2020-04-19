88eec44e097e37ecb3caea3ded4bd3503174f40d
Crowdbotics <team@crowdbotics.com>
2020-04-19 17:00:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ontap_16017/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ontap_16017;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ontap_16017";
+    }
+}
